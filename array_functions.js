ee957ae7fa2cd2d9724f3e89009ba842b7087847
KushalWithK <kushalshah821@gmail.com>
2024-07-17 17:51:03 +0530

    array functions

---
+++ array_functions.js
@@ -0,0 +1,50 @@
+const name = () => {
+
+}
+
+const employees = [
+    {
+        name: 'Kushal',
+        age: 20,
+        designation: 'Web Developer',
+        salary: 15000
+    },
+    {
+        name: 'Anuj',
+        age: 25,
+        designation: 'Web Developer',
+        salary: 25000
+    },
+    {
+        name: 'Aditya',
+        age: 30,
+        designation: 'Web Developer',
+        salary: 7000
+    },
+    {
+        name: 'Mukesh',
+        age: 25,
+        designation: 'Web Developer',
+        salary: 10000
+    },
+    {
+        name: 'Sharukh',
+        age: 18,
+        designation: 'Web Developer',
+        salary: 8000
+    },
+]
+
+// const empName = employees.map((employee) => {
+//     return employee.name
+// })
+
+// console.log(empName);
+
+// employees.forEach(function (employee) {
+//     console.log(employee.name);
+// })
+
+// const filteredEmployee = employees.find((employee) => employee.salary < 10000)
+
+// console.log(filteredEmployee);
--- script.js
+++ script.js
@@ -3,17 +3,17 @@
 */
 
 let employee = [{
-    'name': 'Anuj',
-    'age': 20,
-    designation: 'Lead Web Developer'
+    name: 'Anuj',
+    age: 20,
+    designation: 'Lead Web Developer',
 }, {
-    'name': 'Akhil',
-    'age': 25,
+    name: 'Akhil',
+    age: 25,
     designation: 'Jr. Web Developer',
-    joining: '2022'
-}]
+    joining: '2022',
+},]
 
 for(let i = 0; i < employee.length; i++) {
     emp = employee[i]
-    console.log(emp.name)
-}
+    console.log(emp.age)
+} 
